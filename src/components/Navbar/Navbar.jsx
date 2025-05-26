aadf6121ad0736350037ea71a53d0e1830262cf6
anassultanali <anassultanali@gmail.com>
2025-05-26 03:54:05 +0300

    Navbar Top 16:19

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,8 +1,22 @@
+import { Check, Info } from "lucide-react";
+import { Link } from "react-router";
 
 const Navbar = () => {
     return(
-        <div className="text-red-500">
-            Navbar Page 
+        <div className="navbar_top bg-[#272343] h-[45px] w-full flex items-center  ">
+            <div className="container ">
+                <div className="flex items-center justify-between ">
+                
+                        <p className="flex  gap-2 text-sm font-inter font-normal text-white "><Check /> Free all orders over 50$ </p>
+                    
+                    <div className="navbat_top_right ">
+                        <select defaultValue='server location' className="font-inter ">
+                            <option className="">English</option>
+                            <option className="">العربية</option>
+                        </select>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
